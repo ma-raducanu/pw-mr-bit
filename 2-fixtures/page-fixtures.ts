78dae9b0df213a347e890e0c566ea587a702e10c
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-21 20:51:35 +0300

    Updates to file format

--- 2-fixtures/page-fixtures.ts
+++ 2-fixtures/page-fixtures.ts
@@ -1,5 +1,5 @@
 import { test as base } from '@playwright/test';
-import { CasinoPage } from '../1-page-objects/casino-page';
+import { CasinoPage } from '../1-page-objects/casino-pageaa';
 import { LiveCasinoPage } from '../1-page-objects/live-casino-page';
 import { SportsPage } from '../1-page-objects/sports-page';
 
